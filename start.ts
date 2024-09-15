59617f9babd9b12e8c7c93efde7678cf17582841
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-09-15 15:29:52 +0530

    Deployment

---
+++ start.ts
@@ -0,0 +1,5 @@
+// start.ts
+import { execSync } from 'child_process';
+
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined
+execSync(`next start -p ${port} -H 0.0.0.0`, { stdio: 'inherit' });
